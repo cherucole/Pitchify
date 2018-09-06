ed91e0120e01f37a9cbee12780e7dba25759e77e
cherucole <cheruyotcheruyot@gmail.com>
2018-09-06 15:25:28 +0300

    added migration

--- app/models.py
+++ app/models.py
@@ -1,10 +1,49 @@
 from . import db
+from datetime import datetime
+from flask_login import UserMixin
+
+
 
 
 class User(db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
 
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    # bio = db.Column(db.String(255))
+    # profile_pic_path = db.Column(db.String())
+    # password_secure = db.Column(db.String(255))
+    # reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
     def __repr__(self):
         return f'User {self.username}'
+
+
+
+
+class Pitch(db.Model):
+
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer, primary_key=True)
+    pitch_id = db.Column(db.Integer)
+    pitch_title = db.Column(db.String)
+    pitch_content = db.Column(db.String)
+    posted = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    def __repr__(self):
+        return f'User {self.pitch_content}'
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+
+    def __repr__(self):
+        return f'User {self.name}'
