9a39654a549b73ffecc293175c47f7a1d4e0dad6
cherucole <cheruyotcheruyot@gmail.com>
2018-09-07 14:20:01 +0300

    added form to capture pitch

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,3 +7,10 @@ class CommentForm(FlaskForm):
     # title = StringField('Review title',validators=[Required()])
     comment = TextAreaField('Your Comment')
     submit = SubmitField('Submit')
+
+
+class PitchForm(FlaskForm):
+
+    title = StringField('Pitch title',validators=[Required()])
+    review = TextAreaField('Pitch Body')
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
 from flask import render_template,request,redirect,url_for
 from ..models import Comment,User,Pitch,get_pitch,get_comments
 from . import main
-from .forms import CommentForm
+from .forms import CommentForm, PitchForm
+import markdown2
+
 
 
 
@@ -18,19 +20,21 @@ def index():
 def new_comment(id):
     form = CommentForm()
     pitch = get_pitch(id)
-    comment=get_comments(id)
+    # comment=get_comments(id)
     if form.validate_on_submit():
         title = form.title.data
         comment = form.comment.data
 
         # Updated comment instance
-        new_comment = Comment(id=pitch.id,pitch_title=title,pitch_comment=comment,pitch=pitch)
+        new_comment = Comment(pitch_id=pitch.id, pitch_title=title,pitch_comment=comment)
 
         # save comment method
         new_comment.save_comment()
         return redirect(url_for('.pitch',id = pitch.id ))
 
     # title = f'{comment.comment_content} comment'
+
+    title = 'comment'
     return render_template('new_comment.html', comment_form=form, pitch=pitch,comment=comment)
 
 
@@ -45,3 +49,13 @@ def pitch(id):
     comments = Comment.get_comments(pitch.id)
 
     return render_template('pitch.html',title = title,pitch = pitch,comments = comments)
+
+@main.route('/new_pitch/<int:id>')
+def single_pitch(id):
+    form = PitchForm()
+
+    pitch=Pitch.query.get(id)
+    # if pitch is None:
+    #     abort(404)
+    format_pitch = markdown2.markdown(" ",extras=["code-friendly", "fenced-code-blocks"])
+    return render_template('new_pitch.html',pitch = pitch,format_pitch=format_pitch,pitch_form=form)
