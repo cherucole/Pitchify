2c1458e4ddfb64d0e14f102c52c7dda151e8de2b
cherucole <cheruyotcheruyot@gmail.com>
2018-09-08 12:44:17 +0300

    working on user authentication

--- app/models.py
+++ app/models.py
@@ -1,5 +1,7 @@
 from . import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
+
 # from flask_login import UserMixin
 
 
@@ -14,6 +16,21 @@ class User(db.Model):
     profile_pic_path = db.Column(db.String())
     password_secure = db.Column(db.String(255))
     comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
+
+
+    pass_secure = db.Column(db.String(255))
+
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure, password)
 
     def __repr__(self):
         return f'User {self.username}'
@@ -49,6 +66,9 @@ class Pitch(db.Model):
     pitch_category=db.Column(db.String)
     posted = db.Column(db.DateTime, default=datetime.utcnow)
     pitch_comment = db.Column(db.String)
+
+
+
 
     def save_pitch(self):
         db.session.add(self)
