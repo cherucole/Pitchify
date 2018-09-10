d73d1c0fd3fceaf98f9cc78df31408810a0b6762
cherucole <cheruyotcheruyot@gmail.com>
2018-09-10 11:06:32 +0300

    working on heroku submission

--- app/models.py
+++ app/models.py
@@ -14,11 +14,13 @@ class User(UserMixin,db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     password_secure = db.Column(db.String(255))
-    comments = db.relationship('Comment',backref = 'user',lazy = "dynamic")
+    # comment_id = db.Column(db.Integer, db.ForeignKey("comments.id"))
+
+    comments = db.relationship('Comment',backref = 'username',lazy = "dynamic")
+    # pitch = db.relationship('Comment',backref = 'username',lazy = "dynamic")
 
 
     pass_secure = db.Column(db.String(255))
@@ -43,10 +45,11 @@ class Comment(db.Model):
     __tablename__ = 'comments'
 
     id = db.Column(db.Integer, primary_key=True)
-    comment_id = db.Column(db.Integer)
     comment_content = db.Column(db.String)
     posted = db.Column(db.DateTime, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    pitch_id = db.Column(db.Integer, db.ForeignKey("pitches.id"))
+
 
     def save_comment(self):
         db.session.add(self)
@@ -62,15 +65,13 @@ class Pitch(db.Model):
     __tablename__ = 'pitches'
 
     id = db.Column(db.Integer, primary_key=True)
-    pitch_id = db.Column(db.Integer)
     pitch_title = db.Column(db.String)
     pitch_content = db.Column(db.String)
     pitch_category=db.Column(db.String)
     posted = db.Column(db.DateTime, default=datetime.utcnow)
-    pitch_comment = db.Column(db.String)
     likes=db.Column(db.Integer, default=0)
-
-
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    comments = db.relationship('Comment',backref = 'pitch_content',lazy = "dynamic")
 
 
     def save_pitch(self):
@@ -81,27 +82,6 @@ class Pitch(db.Model):
     def get_pitch(cls, category):
         pitch = Pitch.query.filter_by(category=category).all()
         return pitch
-
-    # def get_pitch(cls, id):
-    #     pitch = Pitch.query.filter_by(pitch_id=id).all()
-    #     return pitch
-
-        # return pitch
-
-    # def __repr__(self):
-    #     return f'User {self.pitch_content}'
-
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-
-    id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    users = db.relationship('User',backref = 'role',lazy="dynamic")
-
-
-    def __repr__(self):
-        return f'User {self.name}'
 
 
 
